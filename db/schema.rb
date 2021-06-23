fa5e2ef32cd40cd747387c09a52cf516452c5208
victorvanoverschelde <victorvanoverschelde@gmail.com>
2021-06-23 14:50:52 +0200

    LES SEEDS

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_06_23_084925) do
-
+ActiveRecord::Schema.define(version: 2021_06_23_085951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -37,6 +34,7 @@ ActiveRecord::Schema.define(version: 2021_06_23_084925) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
 
   create_table "bar_beers", force: :cascade do |t|
     t.integer "quantity"
@@ -113,9 +111,7 @@ ActiveRecord::Schema.define(version: 2021_06_23_084925) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-
   add_foreign_key "bar_beers", "bars"
   add_foreign_key "bar_beers", "beers"
   add_foreign_key "bars", "users"
