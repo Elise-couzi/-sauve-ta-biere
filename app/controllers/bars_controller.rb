e97ada5656bc050d4222955c081db62c2c3fd0ca
Flavie Leroy <leroy.flavie@outlook.fr>
2021-06-23 15:41:43 +0200

    modifs-controller-bar

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,35 +1,25 @@
 class BarsController < ApplicationController
   before_action :set_bar, only: [:show, :edit]
 
-  def index; end
+  def index
+    @bars = Bar.all
+  end
 
   def show; end
-
-  def new
-    @bar = Bar.new
-  end
-
-  def create
-    @bar = Bar.new(bar_params)
-
-    if @bar.save
-      redirect_to bar_path(@bar)
-    else
-      render :new
-    end
-  end
 
   def edit; end
 
   def update
     @bar.update(bar_params)
     redirect_to bar_path(@bar)
+    # authorize @bar
   end
 
   private
 
   def set_bar
     @bar = Bar.find(params[:id])
+    # authorize @bar
   end
 
   def bar_params
