8e1030b0e1c52d56e959c4e6839ceeda0568c5b3
vanessa tsang <va.tsang@yahoo.fr>
2021-06-24 17:58:06 +0200

    TEST FB CONNECT NE PAS MERGE: A VERIF

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+  controllers: { omniauth_callbacks: 'users/omniauth_callbacks' } # pour fb connect
   root to: 'pages#home'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :bars do
     resources :bar_beers, only: [:index, :new, :edit, :destroy]
