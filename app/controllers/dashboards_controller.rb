10cc247340a91a2f9ce94769a7f9f81a86c02d5d
victorvanoverschelde <victorvanoverschelde@gmail.com>
2021-06-25 11:58:34 +0200

    Le dash est ok, a toi de jouer Flav

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,7 +2,7 @@ class DashboardsController < ApplicationController
     before_action :set_bar_beer, only: [:edit, :update, :destroy]
   def index
     @user = current_user
-    @bars = Bar.all
+    @bar = @user.bar
     @bar_beers = policy_scope(BarBeer).where(bar: @bar).includes(:beer)
   end
 #   def index
