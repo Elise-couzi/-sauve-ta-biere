507981647f8265912678eb4e08520220cd923bd7
Flavie Leroy <leroy.flavie@outlook.fr>
2021-06-28 13:54:30 +0200

    on peut editer mais le supprimer marche pas

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   resources :bar_beers, only: [:edit, :update, :destroy]
+
   resources :bars do
-    resources :bar_beers, only: [:index, :new, :create, :edit, :destroy]
+    resources :bar_beers, only: [:index, :new, :create]
   end
 
   get "/beers/new", to: "beers#new"
