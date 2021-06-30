8fefa76c93e99d2e179e7daab4ea74136d918d30
Flavie Leroy <leroy.flavie@outlook.fr>
2021-06-30 16:53:37 +0200

    image ajoutée à users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_30_083853) do
+ActiveRecord::Schema.define(version: 2021_06_30_145208) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,6 +112,7 @@ ActiveRecord::Schema.define(version: 2021_06_30_083853) do
     t.string "first_name"
     t.string "last_name"
     t.string "address"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
