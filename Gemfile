f016882e0d94106e9565a1367825e67fbad55ae5
vanessa tsang <va.tsang@yahoo.fr>
2021-06-30 19:27:36 +0200

    map bar la totale

--- Gemfile
+++ Gemfile
@@ -67,3 +67,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'pundit'
 gem 'money-rails'
 gem 'stripe'
+gem 'geocoder'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_30_083853) do
+ActiveRecord::Schema.define(version: 2021_06_30_135703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 2021_06_30_083853) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "photo"
     t.string "logo"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_bars_on_user_id"
   end
 
@@ -112,6 +114,11 @@ ActiveRecord::Schema.define(version: 2021_06_30_083853) do
     t.string "first_name"
     t.string "last_name"
     t.string "address"
+    t.string "provider"
+    t.string "uid"
+    t.string "facebook_picture_url"
+    t.string "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
