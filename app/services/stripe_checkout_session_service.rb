e2cc077fa922aa43f5d4a1562d7aa9b9793514cd
victorvanoverschelde <victorvanoverschelde@gmail.com>
2021-07-01 15:43:15 +0200

    Alexis m'as dit bien joué pour ce code t'es un bg

--- app/services/stripe_checkout_session_service.rb
+++ app/services/stripe_checkout_session_service.rb
@@ -2,5 +2,9 @@ class StripeCheckoutSessionService
   def call(event)
     order = Order.find_by(checkout_session_id: event.data.object.id)
     order.update(state: 'paid', paid_at: DateTime.now)
+    @order_beers = order.order_beers
+    @order_beers.each do |order_beer|
+      order_beer.bar_beer.update(quantity: order_beer.bar_beer.quantity - order_beer.quantity)
+    end
   end
 end
